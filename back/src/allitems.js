0ca74cd67459f13af5d27981bf44576377bad39f
Gabriel Teles <Gabrielteles001>
2024-09-20 11:50:28 -0300

    feat: Adicionando back

---
+++ back/src/allitems.js
@@ -0,0 +1,26 @@
+const connection = require('./connection')
+
+const getAllItems = async () => {
+    try{
+        const [query] = await connection.execute('SELECT * FROM actionlife.dado_usuario')
+        return query
+    }
+    catch(error){ 
+        throw new Error(`Erro ao buscar itens: ${error.message}`)
+    }
+
+}
+
+async function insertItem(nome, peso, altura, email) {
+    try{
+        const insertQuery = "INSERT INTO dado_usuario (nome, peso, altura, email) VALUES (?, ?, ?, ?)"
+        const values = [nome, peso, altura, email]
+        const [result] = await connection.execute(insertQuery, values)
+        return result
+    }
+    catch(error){ 
+        throw new Error(`Erro ao inserir itens: ${error.message}`)
+    }
+}
+
+module.exports = {getAllItems, insertItem}
